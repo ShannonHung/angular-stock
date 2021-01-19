cd8585bc180bfd3f2356157b60a026978e589f01
Shannon <b10709036@mail.ntust.edu.tw>
2021-01-19 13:04:22 +0800

    add ngClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,11 +10,23 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
   
   public stock: Stock;
+  public stockClasses;
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(){
-    this.stock = new Stock('Test Stock Company', 'TSC', 105, 80);
+    this.stock = new Stock('Test Stock Company', 'TSC', 81, 80);
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    //決定要使用哪一種css: positive | negative | large-change | small-change
+    this.stockClasses = {
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
+    };
   }
 
   //點擊時觸發的函式
